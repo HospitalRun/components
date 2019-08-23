f6f5dff243b8923a172102c9f11280dbd0ec063d
= <jackcmeyer@gmail.com>
2019-08-23 17:05:56 -0500

    adds support for line graphs, bar graphs, and pie graphs for basic graph components

---
+++ src/components/Graph/index.tsx
@@ -0,0 +1,244 @@
+import React, { Component } from 'react'
+import { Graph, Dataset, Axis, HasAxes } from './interfaces'
+import ChartJs, { ChartData, ChartDataSets, Point, CommonAxe } from 'chart.js'
+import { ChartConfiguration, ChartOptions } from 'chart.js'
+
+function getAxisType(type: string): ChartJs.ScaleType {
+  if (type === 'category') {
+    return 'category'
+  } else if (type === 'linear') {
+    return 'linear'
+  } else if (type === 'time') {
+    return 'time'
+  }
+
+  return 'category'
+}
+
+function getAxisLabel(item: Axis): ChartJs.ScaleTitleOptions {
+  return {
+    display: !!item.label,
+    labelString: item.label,
+  }
+}
+
+function axisMapper(item: Axis): ChartJs.CommonAxe {
+  let axis: ChartJs.CommonAxe = {
+    display: !!item.label,
+    scaleLabel: getAxisLabel(item),
+    type: getAxisType(item.type),
+    ticks: {
+      beginAtZero: true,
+    },
+  }
+
+  if (item.type === 'time') {
+    let timeScale: ChartJs.TimeScale = {
+      unit: item.timeFormat,
+      stepSize: item.timeStepSize,
+    }
+
+    axis.time = timeScale
+  }
+
+  return axis
+}
+
+function getAxes(axes: Axis[], stacked: boolean): CommonAxe[] {
+  let chartAxes: CommonAxe[] = []
+  if (axes) {
+    chartAxes = axes.map(axisMapper)
+  }
+
+  chartAxes.forEach(axis => (axis.stacked = stacked))
+
+  return chartAxes
+}
+
+function getChartDataset(dataset: Dataset): ChartDataSets {
+  const label = dataset.label
+  let data: Point[] = []
+  let backgroundColor: string[] = []
+  let borderColor: string[] = []
+
+  dataset.data.forEach(d => {
+    data.push(d.y)
+    backgroundColor.push(d.backgroundColor ? d.backgroundColor : dataset.backgroundColor)
+    borderColor.push(d.borderColor ? d.borderColor : dataset.borderColor)
+  })
+
+  let pointBackgroundColor = backgroundColor
+  let pointBorderColor = borderColor
+
+  return {
+    label,
+    data,
+    backgroundColor,
+    borderColor,
+    pointBackgroundColor,
+    pointBorderColor,
+    borderWidth: 1,
+  }
+}
+
+function getChartDatasets(datasets: Dataset[]): ChartDataSets[] {
+  return datasets.map(getChartDataset)
+}
+
+function getLabelsFromDataset(datasets: Dataset[]) {
+  let labels: string[] = []
+  datasets.forEach(dataset =>
+    dataset.data.forEach(d => {
+      if (!labels.includes(d.x)) {
+        labels.push(d.x)
+      }
+    }),
+  )
+
+  return labels
+}
+
+function getChartData(graph: Graph): ChartData {
+  return {
+    labels: getLabelsFromDataset(graph.datasets),
+    datasets: getChartDatasets(graph.datasets),
+  }
+}
+
+function getCommonChartOptions(graph: Graph): ChartOptions {
+  const options: ChartOptions = {
+    title: {
+      display: !!graph.title,
+      text: graph.title,
+    },
+  }
+
+  if (options.title) {
+    if (graph.titleFontSize) {
+      options.title.fontSize = graph.titleFontSize
+    }
+
+    if (graph.titleFontColor) {
+      options.title.fontColor = graph.titleFontColor
+    }
+  }
+
+  return options
+}
+
+function getCommonChartConfigurations(type: string, graph: Graph): ChartConfiguration {
+  return {
+    type,
+    data: getChartData(graph),
+    options: getCommonChartOptions(graph),
+  }
+}
+
+interface IBarGraph extends Graph, HasAxes {
+  /** Determines if the bar graph should be displayed in a horizontal manner */
+  horizontal: boolean
+  /** Determines if the bar graph should be displayed as a stacked bar graph */
+  stacked: boolean
+}
+
+class BarGraph extends Component<IBarGraph, {}> {
+  graph: any
+  chart: any
+
+  componentDidMount() {
+    const { horizontal, stacked, xAxes, yAxes } = this.props
+    const type = horizontal ? 'horizontalBar' : 'bar'
+    const config = getCommonChartConfigurations(type, this.props)
+
+    if (config && config.options) {
+      let scales = undefined
+      if (!horizontal) {
+        scales = {
+          xAxes: getAxes(xAxes, stacked),
+          yAxes: getAxes(yAxes, stacked),
+        }
+      } else {
+        scales = {
+          xAxes: getAxes(yAxes, stacked),
+          yAxes: getAxes(xAxes, stacked),
+        }
+      }
+
+      config.options.scales = scales
+    }
+
+    this.graph = new ChartJs(this.chart, config)
+  }
+
+  render() {
+    return <canvas ref={chart => (this.chart = chart)}></canvas>
+  }
+}
+
+interface IPieGraph extends Graph {
+  /** Determines if the pie graph should be displayed with a doughnut */
+  doughnut: boolean
+}
+
+class PieGraph extends Component<IPieGraph, {}> {
+  graph: any
+  chart: any
+
+  componentDidMount() {
+    const { doughnut } = this.props
+    const type = doughnut ? 'doughnut' : 'pie'
+    const config = getCommonChartConfigurations(type, this.props)
+    this.graph = new ChartJs(this.chart, config)
+  }
+
+  render() {
+    return <canvas ref={chart => (this.chart = chart)}></canvas>
+  }
+}
+
+interface ILineGraph extends Graph, HasAxes {
+  /** Determines if the area under the line should be filled with the background color from the dataset */
+  fill: boolean
+  /** Determines if the the Line Graphs should be displayed in a stacked manner */
+  stacked: boolean
+}
+
+class LineGraph extends Component<ILineGraph, {}> {
+  graph: any
+  chart: any
+
+  componentDidMount() {
+    const type = 'line'
+    let fill = false
+
+    if (this.props.fill) {
+      fill = this.props.fill
+    }
+
+    const config = getCommonChartConfigurations(type, this.props)
+    if (config && config.data && config.data.datasets) {
+      for (let i = 0; i < this.props.datasets.length; i++) {
+        config.data.datasets[i].fill = fill
+        config.data.datasets[i].backgroundColor = this.props.datasets[i].backgroundColor
+        config.data.datasets[i].borderColor = this.props.datasets[i].borderColor
+      }
+    }
+
+    if (config && config.options) {
+      let scales = {
+        xAxes: getAxes(this.props.xAxes, false),
+        yAxes: getAxes(this.props.yAxes, this.props.stacked),
+      }
+
+      config.options.scales = scales
+    }
+
+    this.graph = new ChartJs(this.chart, config)
+  }
+
+  render() {
+    return <canvas ref={chart => (this.chart = chart)}></canvas>
+  }
+}
+
+export { BarGraph, PieGraph, LineGraph }
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
 export * from './components/Toaster'
 export * from './components/Spinner'
 export * from './components/Confirm'
+export * from './components/Graph';
