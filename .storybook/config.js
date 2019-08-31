473b9822ccaa549f48601ac7a550aa5484c57b2d
Matteo Vivona <kapa89@me.com>
2019-08-31 13:54:33 +0200

    Updates storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,18 +3,9 @@ import { themes } from '@storybook/theming'
 
 addParameters({
   options: {
-    /**
-     * Name to display in the top left corner
-     * @type {String}
-     */
     name: 'HospitalRun',
-    /**
-     * URL for name in top left corner to link to
-     * @type {String}
-     */
     url: 'https://github.com/HospitalRun/components',
-
-    theme: themes.dark,
+    theme: themes,
   },
 })
 
@@ -22,6 +13,7 @@ addParameters({
 const req = require.context('../stories', true, /\.stories\.tsx$/)
 
 function loadStories() {
+  require('../stories/welcome')
   req.keys().forEach(filename => req(filename))
 }
 
---
+++ stories/welcome.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+
+storiesOf('Welcome', module).add('Introduction', () => <h1>Welcome to HospitalRun Storybook</h1>)
