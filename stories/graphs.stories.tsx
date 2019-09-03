83672c93d8887dae3a08a6f9a2b92e378cdfa43d
Matteo Vivona <kapa89@me.com>
2019-09-03 10:20:39 +0200

    perf: adds test and jest-canvas-mock

--- stories/graphs.stories.tsx
+++ stories/graphs.stories.tsx
@@ -5,63 +5,80 @@ import { storiesOf } from '@storybook/react'
 import { LineGraph, PieGraph, BarGraph } from '@hospitalrun-org/components'
 
 storiesOf('Graphs', module)
-  .add('Bar Graph', () => 
+  .add('Bar Graph', () => (
     <BarGraph
-      title="Test Graph" 
-      datasets={[{
-        backgroundColor: "blue",
-        label: "Months",
-        data: [{x: 'January', y: 12}, {x: 'February', y: 11}, {x: 'March', y: 10}] ,
-      }]}
-      xAxes={[{label: 'Months', type: 'category'}]} 
-      yAxes={[{label: 'Numbers', type:'linear'}]}
+      title="Test Graph"
+      datasets={[
+        {
+          backgroundColor: 'blue',
+          label: 'Months',
+          data: [{ x: 'January', y: 12 }, { x: 'February', y: 11 }, { x: 'March', y: 10 }],
+        },
+      ]}
+      xAxes={[{ label: 'Months', type: 'category' }]}
+      yAxes={[{ label: 'Numbers', type: 'linear' }]}
     />
-  )
-  .add('Bar Graph - Horizontal', () => 
-    <BarGraph 
+  ))
+  .add('Bar Graph - Horizontal', () => (
+    <BarGraph
       horizontal
       title="Test Graph"
-      datasets={[{
-        backgroundColor: "blue",
-        label: "Months",
-        data: [{x: 'January', y: 12}, {x: 'February', y: 11}, {x: 'March', y: 10}]   
-      }]}
-      xAxes={[{label: 'Months', type: 'category'}]} 
-      yAxes={[{label: 'Numbers', type:'linear'}]} 
+      datasets={[
+        {
+          backgroundColor: 'blue',
+          label: 'Months',
+          data: [{ x: 'January', y: 12 }, { x: 'February', y: 11 }, { x: 'March', y: 10 }],
+        },
+      ]}
+      xAxes={[{ label: 'Months', type: 'category' }]}
+      yAxes={[{ label: 'Numbers', type: 'linear' }]}
     />
-  )
-  .add('Line Graph', () => 
-    <LineGraph 
+  ))
+  .add('Line Graph', () => (
+    <LineGraph
       title="Test Graph"
-      datasets={[{
-        borderColor: "red",
-        backgroundColor: "blue",
-        label: "Months",
-        data: [{x: 'January', y: 12}, {x: 'February', y: 11}, {x: 'March', y: 10}]   
-      }]}
-      xAxes={[{label: 'Months', type: 'category'}]} 
-      yAxes={[{label: 'Numbers', type:'linear'}]} 
+      datasets={[
+        {
+          borderColor: 'red',
+          backgroundColor: 'blue',
+          label: 'Months',
+          data: [{ x: 'January', y: 12 }, { x: 'February', y: 11 }, { x: 'March', y: 10 }],
+        },
+      ]}
+      xAxes={[{ label: 'Months', type: 'category' }]}
+      yAxes={[{ label: 'Numbers', type: 'linear' }]}
     />
-  )
-  .add('Pie Graph', () => 
-    <PieGraph 
-      title="Pie Graph" 
-      datasets={[{
-        backgroundColor: "blue",
-        label: "Months",
-        data: [{x: 'January', y: 12, backgroundColor: "red"}, {x: 'February', y: 11, backgroundColor: "blue"}, {x: 'March', y: 10, backgroundColor: "yellow"}]   
-      }]}
+  ))
+  .add('Pie Graph', () => (
+    <PieGraph
+      title="Pie Graph"
+      datasets={[
+        {
+          backgroundColor: 'blue',
+          label: 'Months',
+          data: [
+            { x: 'January', y: 12, backgroundColor: 'red' },
+            { x: 'February', y: 11, backgroundColor: 'blue' },
+            { x: 'March', y: 10, backgroundColor: 'yellow' },
+          ],
+        },
+      ]}
     />
-  )
-  .add('Pie Graph - Doughnut', () => 
-    <PieGraph 
+  ))
+  .add('Pie Graph - Doughnut', () => (
+    <PieGraph
       doughnut
-      title="Pie Graph - Doughnut" 
-      datasets={[{
-        backgroundColor: "blue",
-        label: "Months",
-        data: [{x: 'January', y: 12, backgroundColor: "red"}, {x: 'February', y: 11, backgroundColor: "blue"}, {x: 'March', y: 10, backgroundColor: "yellow"}]   
-      }]}
+      title="Pie Graph - Doughnut"
+      datasets={[
+        {
+          backgroundColor: 'blue',
+          label: 'Months',
+          data: [
+            { x: 'January', y: 12, backgroundColor: 'red' },
+            { x: 'February', y: 11, backgroundColor: 'blue' },
+            { x: 'March', y: 10, backgroundColor: 'yellow' },
+          ],
+        },
+      ]}
     />
-  )
-
+  ))
