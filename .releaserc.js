ea435037c098ea2148b82926daf7ba84cc22065f
Matteo Vivona <kapa89@me.com>
2019-09-10 17:32:06 +0200

    :green_heart: (semantic-release) Updates releaserc.js

--- .releaserc.js
+++ .releaserc.js
@@ -50,10 +50,7 @@ module.exports = {
         assets: ['CHANGELOG.md', 'package.json', 'packages'],
         message: ':bookmark: (release) ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
-    ],
-    ["@semantic-release/npm", {
-      "npmPublish": false,
-    }
+      '@semantic-release/npm',
     ],
   ],
 }
