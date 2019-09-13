0ce1a3a9917c805d866d19f71802ece923eb0ad0
Stefano Casasola <stepanello@gmail.com>
2019-09-13 17:31:45 +0200

    :recycle: (storybook) Improves storybook
    Removes separate css file, adds decorators to stories, adds Toaster stories

--- stories/spinner.stories.tsx
+++ stories/spinner.stories.tsx
@@ -4,66 +4,67 @@ import { storiesOf } from '@storybook/react'
 
 import { Spinner } from '@hospitalrun-org/components'
 
-import './styles.css'
-
 storiesOf('Spinner', module)
   .addParameters({
     info: {
       inline: true,
     },
   })
+  .addDecorator(storyFn => (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>{storyFn()}</div>
+  ))
   .add('BarLoader', () => (
-    <div className="spinner">
+    <div>
       <Spinner type="BarLoader" loading={true} color={'blue'} size={[50, 15]} />
     </div>
   ))
   .add('BeatLoader', () => (
-    <div className="spinner">
+    <div>
       <Spinner type="BeatLoader" loading={true} color={'blue'} size={20} />
     </div>
   ))
   .add('BounceLoader', () => (
-    <div className="spinner">
+    <div>
       <Spinner type="BounceLoader" loading={true} color={'blue'} size={40} />
     </div>
   ))
   .add('ClimbingBoxLoader', () => (
-    <div className="spinner">
+    <div>
       <Spinner type="ClimbingBoxLoader" loading={true} color={'blue'} size={15} />
     </div>
   ))
   .add('ClipLoader', () => (
-    <div className="spinner">
+    <div>
       <Spinner type="ClipLoader" loading={true} color={'blue'} size={30} />
     </div>
   ))
   .add('DotLoader', () => (
-    <div className="spinner">
+    <div>
       <Spinner type="DotLoader" loading={true} color={'blue'} size={30} />
     </div>
   ))
   .add('FadeLoader', () => (
-    <div className="spinner">
+    <div>
       <Spinner type="FadeLoader" loading={true} color={'blue'} size={[5, 15]} />
     </div>
   ))
   .add('PulseLoader', () => (
-    <div className="spinner">
+    <div>
       <Spinner type="PulseLoader" loading={true} color={'blue'} size={20} />
     </div>
   ))
   .add('RotateLoader', () => (
-    <div className="spinner">
+    <div>
       <Spinner type="RotateLoader" loading={true} color={'blue'} size={15} />
     </div>
   ))
   .add('ScaleLoader', () => (
-    <div className="spinner">
+    <div>
       <Spinner type="ScaleLoader" loading={true} color={'blue'} size={[10, 25]} />
     </div>
   ))
   .add('SyncLoader', () => (
-    <div className="spinner">
+    <div>
       <Spinner type="SyncLoader" loading={true} color={'blue'} size={20} />
     </div>
   ))
---
+++ stories/toaster.stories.tsx
@@ -0,0 +1,67 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+
+import { Toast, Toaster } from '@hospitalrun-org/components'
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+storiesOf('Toaster', module)
+  .addParameters({
+    info: {
+      inline: true,
+    },
+  })
+  .addDecorator(storyFn => <div style={{ textAlign: 'center' }}>{storyFn()}</div>)
+  .add('Success', () => (
+    <div>
+      <Button
+        color="primary"
+        onClick={() => {
+          Toast('success', 'This is a toaster!', 'Success')
+        }}
+      >
+        Click me!
+      </Button>
+      <Toaster autoClose={3000} hideProgressBar={true} draggable={true} />
+    </div>
+  ))
+  .add('Info', () => (
+    <div>
+      <Button
+        color="primary"
+        onClick={() => {
+          Toast('info', 'This is a toaster!', 'Info')
+        }}
+      >
+        Click me!
+      </Button>
+      <Toaster autoClose={3000} hideProgressBar={true} draggable={true} />
+    </div>
+  ))
+  .add('Warning', () => (
+    <div>
+      <Button
+        color="primary"
+        onClick={() => {
+          Toast('warning', 'This is a toaster!', 'Warning')
+        }}
+      >
+        Click me!
+      </Button>
+      <Toaster autoClose={3000} hideProgressBar={true} draggable={true} />
+    </div>
+  ))
+  .add('Error', () => (
+    <div>
+      <Button
+        color="primary"
+        onClick={() => {
+          Toast('error', 'This is a toaster!', 'Error')
+        }}
+      >
+        Click me!
+      </Button>
+      <Toaster autoClose={3000} hideProgressBar={true} draggable={true} />
+    </div>
+  ))
