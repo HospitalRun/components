ac5b9855f1984a3f8e93fb8eaf284f9a94631cd8
Jack Meyer <jackcmeyer@gmail.com>
2019-09-26 00:45:23 -0500

    refactor(icons): moves icon set import and removes unused dependency
    #19

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,7 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
+
 export * from './components/Toaster'
 export * from './components/Spinner'
 export * from './components/Confirm'
