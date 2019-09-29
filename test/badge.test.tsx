9b52c431fd907dd7aaec7b11533e7b2dd7d9d644
Jack Meyer <jackcmeyer@gmail.com>
2019-09-28 22:27:35 -0500

    feat(badges): adds badges
    #22

---
+++ test/badge.test.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react'
+import { shallow } from 'enzyme'
+import { Badge } from '../src/components/Badge'
+import { Badge as BootstrapBadge } from 'react-bootstrap'
+
+describe('Badge', () => {
+  it('Badge renders itself without crashing', () => {
+    const badgeWrapper = shallow(<Badge />)
+    expect(badgeWrapper.find(BootstrapBadge)).toHaveLength(1)
+  })
+
+  it('Badge defaults color to primary', () => {
+    const badgeWrapper = shallow(<Badge />)
+    const bootstrapBadge = badgeWrapper.find(BootstrapBadge)
+    expect(bootstrapBadge.props().variant).toEqual('primary')
+  })
+
+  it('Badge can change colors', () => {
+    const badgeWrapper = shallow(<Badge color="secondary" />)
+    const bootstrapBadge = badgeWrapper.find(BootstrapBadge)
+    expect(bootstrapBadge.props().variant).toEqual('secondary')
+  })
+
+  it('Badge can redner children elements', () => {
+    const badgeWrapper = shallow(<Badge>Hello World</Badge>)
+    const bootstrapBadge = badgeWrapper.find(BootstrapBadge)
+    expect(bootstrapBadge.text()).toEqual('Hello World')
+  })
+})
