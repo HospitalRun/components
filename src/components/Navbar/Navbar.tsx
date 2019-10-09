753c6c422935c1a6b4c6fa70e512430a5c8e864d
Edson <efkzhu@myseneca.ca>
2019-10-09 00:48:13 -0400

    fix(navbar): changed the code to be more dynamically

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,43 +2,106 @@ import React, { Component } from 'react'
 import NavbarRB from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Form from 'react-bootstrap/Form'
-import FormControl from 'react-bootstrap/FormControl'
-import Button from 'react-bootstrap/Button'
+import FormControl, { FormControlProps } from 'react-bootstrap/FormControl'
 import NavDropdown from 'react-bootstrap/NavDropdown'
+import { ReplaceProps, BsPrefixProps } from 'react-bootstrap/helpers'
 
-interface Props {
+import { Button } from '../../../src'
+
+interface Props extends React.Props<any> {
   /** Determines the navbar background color */
   bg?: 'light' | 'dark'
   /** Determines the letters color. It should be combined with the background color (bg) */
   variant?: 'light' | 'dark'
+  /** Determines the links names and their path. The array is 2 dimensions.
+   * The first array (Arr[i]) must contain the links data individually by link.
+   * The first element of the second arry (Arr[i][0]) must be the link name.
+   * The second element of the second arry (Arr[i][1]) must be the link path.
+   * The third and all the even numbers of the second arry (Arr[i][2], Arr[i][4], ...) must be the sub-link name, if there is any.
+   * The forth and all the odds numbers of the second arry (Arr[i][3], Arr[i][5], ...) must be the sub-link path, if there is any. */
+  links: string[][]
+  /** Determines the hospital/clinic name to be shown at the navbar */
+  brand: string
+  /** Determines the hospital/clinic icon/image/text which will be shown at the navbar */
+  src?: string
+  /** Defines the button variant. By default is primary */
+  buttonColor?:
+    | 'primary'
+    | 'secondary'
+    | 'success'
+    | 'warning'
+    | 'danger'
+    | 'info'
+    | 'light'
+    | 'dark'
+  /** Handles the on click search button event */
+  onSeachButtonClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
+  /** Handles the on change search form event */
+  onSearchTextBoxChange: (
+    event: React.FormEvent<ReplaceProps<'input', BsPrefixProps<'input'> & FormControlProps>>,
+  ) => void
 }
 
 /**
  * Used to redirect users to the main topics.
  */
-class Navbar extends Component<Props, {}> {
+class Navbar extends Component<Props, any> {
+  createDropdwonElem = (linkEle: any, index: any) => {
+    const subLink = []
+    for (let i = 2; i < linkEle.length - 1; i += 2) {
+      subLink.push(
+        <NavDropdown.Item href={linkEle[i + 1]} key={index * index * i}>
+          {linkEle[i]}
+        </NavDropdown.Item>,
+      )
+    }
+    return subLink
+  }
+
   render() {
+    const buttonColor = this.props.buttonColor ? this.props.buttonColor : 'primary'
+    let img
+
+    if (this.props.src) {
+      img = <img src={this.props.src} width="28" height="28" className="d-inline-block align-top" />
+    } else {
+      img = ''
+    }
+
     return (
       <NavbarRB
         bg={this.props.bg ? this.props.bg : 'dark'}
         variant={this.props.variant ? this.props.variant : 'dark'}
       >
-        <NavbarRB.Brand href="#">{' HospitalRun'}</NavbarRB.Brand>
+        <NavbarRB.Brand href="/">
+          {img}
+          {` ${this.props.brand}`}
+        </NavbarRB.Brand>
         <NavbarRB.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#">Elem1</Nav.Link>
-            <Nav.Link href="#">Elem2</Nav.Link>
-            <Nav.Link href="#">Elem3</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action1</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Action2</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Action3</NavDropdown.Item>
-            </NavDropdown>
+            {this.props.links.map((link, index) => {
+              return link.length == 2 ? (
+                <Nav.Link href={link[1]} key={index}>
+                  {link[0]}
+                </Nav.Link>
+              ) : (
+                <NavDropdown title={link[0]} id="collasible-nav-dropdown" key={index}>
+                  {this.createDropdwonElem(link, index)}
+                </NavDropdown>
+              )
+            })}
           </Nav>
           <Nav>
             <Form inline>
-              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-              <Button>Search</Button>
+              <FormControl
+                type="text"
+                placeholder="Search"
+                className="mr-sm-2"
+                onChange={this.props.onSearchTextBoxChange}
+              />
+              <Button color={buttonColor} onClick={this.props.onSeachButtonClick}>
+                Search
+              </Button>
             </Form>
           </Nav>
         </NavbarRB.Collapse>
--- stories/navbar.stories.tsx
+++ stories/navbar.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { storiesOf } from '@storybook/react'
 
-import { Navbar } from '../src'
+import { Navbar, Toaster, Toast } from '../src'
 
 storiesOf('Navbar', module)
   .addParameters({
@@ -13,9 +13,48 @@ storiesOf('Navbar', module)
   .addDecorator(storyFn => <div style={{ textAlign: 'center' }}>{storyFn()}</div>)
   .add('Navbar', () => (
     <div>
-      <Navbar />
+      <Navbar
+        brand="Hospital"
+        buttonColor="success"
+        onSeachButtonClick={() => {
+          Toast('success', 'Button Clicked!!', 'Success')
+        }}
+        onSearchTextBoxChange={() => {
+          Toast('success', 'Search box changed!!', 'Success')
+        }}
+        links={[
+          ['Elem1', '/elem1Path'],
+          ['Elem2', '/elem2Path'],
+          [
+            'Elem3',
+            '/elem3Path',
+            'Elem3Sub1',
+            '/sub1Path',
+            'Elem3Sub2',
+            '/sub2Path',
+            'Elem3Sub3',
+            'sub3Path',
+          ],
+        ]}
+      />
+
       <br />
-      <Navbar bg="light" variant="light" />
+
+      <Navbar
+        brand="HospitalRun"
+        src="https://raw.githubusercontent.com/HospitalRun/hospitalrun.github.io/master/favicon.png"
+        bg="light"
+        variant="light"
+        onSeachButtonClick={() => {
+          Toast('success', 'Button Clicked!!', 'Success')
+        }}
+        onSearchTextBoxChange={() => {
+          Toast('success', 'Search box changed!!', 'Success')
+        }}
+        links={[['Elem1', '/elem1Path'], ['Elem2', '/elem2Path']]}
+      />
       <br />
+
+      <Toaster autoClose={800} hideProgressBar={true} draggable={true} />
     </div>
   ))
