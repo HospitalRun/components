0c01177470bc89f0abc187fc0af7c258ad52c3e5
Matteo Vivona <kapa89@me.com>
2019-10-11 16:51:17 +0200

    test(coveralls): updates import directory

--- test/badge.test.tsx
+++ test/badge.test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { shallow } from 'enzyme'
-import { Badge } from '../dist'
+import { Badge } from '../src'
 import { Badge as BootstrapBadge } from 'react-bootstrap'
 
 describe('Badge', () => {
