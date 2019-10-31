694019804ee04126a156695c2ef5a3c68ce8f342
Jack Meyer <jackcmeyer@gmail.com>
2019-10-30 23:39:56 -0500

    refactor(spinners): use functional components

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import * as Spinners from 'react-spinners'
 import { SpinnerType, SpinnerSizeUnit } from './interfaces'
 import * as strings from './strings'
@@ -27,58 +27,54 @@ interface Props {
 /**
  * A customizable spinner component. It's a wrapper component built upon react-spinners.
  */
-class Spinner extends Component<Props, {}> {
-  commonStyles = {
-    loading: this.props.loading,
-    color: this.props.color ? this.props.color : 'grey',
-    margin: this.props.margin ? this.props.margin : '2px',
+const Spinner: React.FunctionComponent<Props> = props => {
+  const { loading, color, margin, size, sizeUnit, type } = props
+
+  const commonStyles = {
+    loading: loading,
+    color: color ? color : 'grey',
+    margin: margin ? margin : '2px',
   }
 
-  loaderStyles1 = {
-    ...this.commonStyles,
-    size: this.props.size ? (this.props.size as number) : 15,
-    sizeUnit: this.props.sizeUnit ? (this.props.sizeUnit as SpinnerSizeUnit) : 'px',
+  const loaderStyles1 = {
+    ...commonStyles,
+    size: size ? (props.size as number) : 15,
+    sizeUnit: sizeUnit ? (sizeUnit as SpinnerSizeUnit) : 'px',
   }
 
-  loaderStyles2 = {
-    ...this.commonStyles,
-    width: this.props.size ? (this.props.size as [number, number])[0] : 5,
-    height: this.props.size ? (this.props.size as [number, number])[1] : 15,
-    widthUnit: this.props.sizeUnit
-      ? (this.props.sizeUnit as [SpinnerSizeUnit, SpinnerSizeUnit])[0]
-      : 'px',
-    heightUnit: this.props.sizeUnit
-      ? (this.props.sizeUnit as [SpinnerSizeUnit, SpinnerSizeUnit])[1]
-      : 'px',
+  const loaderStyles2 = {
+    ...commonStyles,
+    width: size ? (size as [number, number])[0] : 5,
+    height: size ? (size as [number, number])[1] : 15,
+    widthUnit: sizeUnit ? (sizeUnit as [SpinnerSizeUnit, SpinnerSizeUnit])[0] : 'px',
+    heightUnit: sizeUnit ? (sizeUnit as [SpinnerSizeUnit, SpinnerSizeUnit])[1] : 'px',
   }
 
-  render() {
-    switch (this.props.type) {
-      case 'BarLoader':
-        return <Spinners.BarLoader {...this.loaderStyles2} />
-      case 'BeatLoader':
-        return <Spinners.BeatLoader {...this.loaderStyles1} />
-      case 'BounceLoader':
-        return <Spinners.BounceLoader {...this.loaderStyles1} />
-      case 'ClimbingBoxLoader':
-        return <Spinners.ClimbingBoxLoader {...this.loaderStyles1} />
-      case 'ClipLoader':
-        return <Spinners.ClipLoader {...this.loaderStyles1} />
-      case 'DotLoader':
-        return <Spinners.DotLoader {...this.loaderStyles1} />
-      case 'FadeLoader':
-        return <Spinners.FadeLoader {...this.loaderStyles2} />
-      case 'PulseLoader':
-        return <Spinners.PulseLoader {...this.loaderStyles1} />
-      case 'RotateLoader':
-        return <Spinners.RotateLoader {...this.loaderStyles1} />
-      case 'ScaleLoader':
-        return <Spinners.ScaleLoader {...this.loaderStyles2} />
-      case 'SyncLoader':
-        return <Spinners.SyncLoader {...this.loaderStyles1} />
-      default:
-        return <div>{strings.invalidSpinner}</div>
-    }
+  switch (type) {
+    case 'BarLoader':
+      return <Spinners.BarLoader {...loaderStyles2} />
+    case 'BeatLoader':
+      return <Spinners.BeatLoader {...loaderStyles1} />
+    case 'BounceLoader':
+      return <Spinners.BounceLoader {...loaderStyles1} />
+    case 'ClimbingBoxLoader':
+      return <Spinners.ClimbingBoxLoader {...loaderStyles1} />
+    case 'ClipLoader':
+      return <Spinners.ClipLoader {...loaderStyles1} />
+    case 'DotLoader':
+      return <Spinners.DotLoader {...loaderStyles1} />
+    case 'FadeLoader':
+      return <Spinners.FadeLoader {...loaderStyles2} />
+    case 'PulseLoader':
+      return <Spinners.PulseLoader {...loaderStyles1} />
+    case 'RotateLoader':
+      return <Spinners.RotateLoader {...loaderStyles1} />
+    case 'ScaleLoader':
+      return <Spinners.ScaleLoader {...loaderStyles2} />
+    case 'SyncLoader':
+      return <Spinners.SyncLoader {...loaderStyles1} />
+    default:
+      return <div>{strings.invalidSpinner}</div>
   }
 }
 
