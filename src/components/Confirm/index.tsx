717f5675a2291973a38b8fb80971c341c3ca1b67
Stefano Casasola <stepanello@gmail.com>
2019-11-08 18:16:24 +0100

    refactor(confirm): removes confirm component
    Due to the new Modal component, Confirm is now unnecessary and should be removed. Every
    functionality can be replicated without external deps and css.

--- src/components/Confirm/index.tsx
+++
@@ -1,8 +0,0 @@
-import { confirmAlert } from 'react-confirm-alert'
-import Modal from './modal'
-import { ConfirmProps } from './interfaces'
-
-export const Confirm = (props: ConfirmProps) =>
-  confirmAlert({
-    customUI: Modal(props),
-  })
