304666f62191358ff946782e4c74e42a4b59e666
Bruno Costa <brunocgmxus@gmail.com>
2019-12-18 20:00:54 -0300

    feat(dropdown): add dropdown component
    Add dropdown component, initial structure, test, documentation and story
    
    fix #58

---
+++ test/dropdown.test.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import { shallow } from 'enzyme'
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import { Dropdown } from '../src'
+
+describe('Dropdown', () => {
+  it('Dropdown renders itself without crashing', () => {
+    const dropdownWrapper = shallow(
+      <Dropdown size="sm" text="Dropdown Text" items={[{ text: 'Link' }]} />,
+    )
+    expect(dropdownWrapper.find(DropdownButton)).toHaveLength(1)
+  })
+})
