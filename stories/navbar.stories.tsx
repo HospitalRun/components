c569ce669983fc062deebd79509c84d4944d0811
ocBruno <brunocgmxus@gmail.com>
2020-01-25 15:35:18 -0300

    fix(navbar): remove test story
    fix #228

--- stories/navbar.stories.tsx
+++ stories/navbar.stories.tsx
@@ -128,57 +128,6 @@ storiesOf('Navbar', module)
       ]}
     />
   ))
-  .add('Dark navbar w/ link 2list', () => (
-    <Navbar
-      bg="dark"
-      variant="dark"
-      navItems={[
-        {
-          type: 'icon',
-          src:
-            'https://raw.githubusercontent.com/HospitalRun/hospitalrun.github.io/master/favicon.png',
-          onClick: () => {
-            Toast('success', 'Icon clicked!!', 'Success')
-          },
-        },
-        {
-          type: 'header',
-          label: 'HospitalRun',
-          onClick: () => {
-            Toast('success', 'Header clicked!!', 'Success')
-          },
-        },
-        {
-          type: 'link',
-          label: 'Link',
-          onClick: () => {
-            Toast('success', 'Link clicked!!', 'Success')
-          },
-        },
-        {
-          type: 'link-list',
-          label: 'Link List',
-          onClick: () => {
-            Toast('success', 'Link list clicked!!', 'Success')
-          },
-          children: [],
-        },
-        {
-          type: 'search',
-          placeholderText: 'Custom',
-          className: 'ml-auto',
-          buttonText: 'Text',
-          buttonColor: 'secondary',
-          onClickButton: () => {
-            Toast('success', 'Search button clicked!!', 'Success')
-          },
-          onChangeInput: () => {
-            Toast('success', 'Search box changed!!', 'Success')
-          },
-        },
-      ]}
-    />
-  ))
   .add('Navbar w/ icons', () => (
     <Navbar
       navItems={[
