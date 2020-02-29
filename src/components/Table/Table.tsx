aebda85fd6dbc5000b6649396d1256f95546bf64
Stefano Casasola <stepanello@gmail.com>
2020-02-29 10:55:30 +0100

    feat: adds pagination

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useTable, useFilters, useSortBy, Row } from 'react-table'
+import { useTable, useFilters, useSortBy, Row, usePagination } from 'react-table'
 import { generateColumns } from './helper'
 
 interface MyProps {
@@ -12,57 +12,120 @@ function Table({ data, tableProperties }: MyProps) {
   const columns = generateColumns(tableProperties.columns)
   console.log('COLUMNS', columns)
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable(
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    prepareRow,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  } = useTable(
     {
       columns,
       data,
+      initialState: { pageIndex: 0 },
     },
     useFilters,
     useSortBy,
+    usePagination,
   )
 
   return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map((headerGroup: any) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column: any) => (
-              <th
-                {...column.getHeaderProps()}
-                className={column.headerClassName ? column.headerClassName : ''}
-              >
-                <div {...(column.disableSorting ? null : column.getSortByToggleProps())}>
-                  {/* {console.log('column', column)} */}
-                  {column.render('Header')}
-                  <span>{column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : ''}</span>
-                </div>
-
-                <div>{!column.disableFiltering && column.render('Filter')}</div>
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row: Row) => {
-          // console.log(row)
-          prepareRow(row)
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell: any) => (
-                // console.log('cell.getCellProps()', cell)
-                <td
-                  {...cell.getCellProps()}
-                  className={cell.column.className ? cell.column.className : ''}
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup: any) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column: any) => (
+                <th
+                  {...column.getHeaderProps()}
+                  className={column.headerClassName ? column.headerClassName : ''}
                 >
-                  {cell.render('Cell')}
-                </td>
+                  <div {...(column.disableSorting ? null : column.getSortByToggleProps())}>
+                    {/* {console.log('column', column)} */}
+                    {column.render('Header')}
+                    <span>{column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : ''}</span>
+                  </div>
+
+                  <div>{!column.disableFiltering && column.render('Filter')}</div>
+                </th>
               ))}
             </tr>
-          )
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row: Row) => {
+            // console.log(row)
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell: any) => (
+                  // console.log('cell.getCellProps()', cell)
+                  <td
+                    {...cell.getCellProps()}
+                    className={cell.column.className ? cell.column.className : ''}
+                  >
+                    {cell.render('Cell')}
+                  </td>
+                ))}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      <div className="pagination">
+        <button type="button" onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
+        <button type="button" onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>{' '}
+        <button type="button" onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>{' '}
+        <button type="button" onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>{' '}
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        <span>
+          | Go to page:{' '}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const p = e.target.value ? Number(e.target.value) - 1 : 0
+              gotoPage(p)
+            }}
+            style={{ width: '100px' }}
+          />
+        </span>{' '}
+        <select
+          value={pageSize}
+          onChange={(e) => {
+            setPageSize(Number(e.target.value))
+          }}
+        >
+          {[10, 20, 30, 40, 50].map((pageS) => (
+            <option key={pageS} value={pageS}>
+              Show {pageS}
+            </option>
+          ))}
+        </select>
+      </div>
+    </>
   )
 }
 
