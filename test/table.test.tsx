dada45c796808f7f613c2c7841c10ae7572b18c0
Stefano Casasola <stepanello@gmail.com>
2020-02-29 16:38:00 +0100

    feat: adds more testing

--- test/table.test.tsx
+++ test/table.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
 import { Table } from '../src'
 
 const data = [
@@ -24,7 +24,7 @@ const data = [
     lastName: 'Gutemberg',
     age: 22,
     status: 'married',
-    admin: false,
+    admin: true,
     date: new Date().toString(),
   },
 ]
@@ -36,13 +36,44 @@ const tableProperties: any = {
       accessor: 'firstName',
       type: 'string',
       title: 'Nome',
+      className: 'pl-3 fixoverflow',
+    },
+    {
+      accessor: 'admin',
+      type: 'boolean',
+      title: 'Admin',
+      className: 'pl-3 fixoverflow',
     },
   ],
 }
 
 describe('Table', () => {
   it('Table renders itself without crashing', () => {
-    const tableWrapper = shallow(<Table data={data} tableProperties={tableProperties} />)
-    expect(tableWrapper.find('table')).toHaveLength(1)
+    const tableWrapper = mount(<Table data={data} tableProperties={tableProperties} />)
+    expect(tableWrapper.find('table')).toBeTruthy()
+  })
+
+  it('Data cell is rendered correctly', () => {
+    const tableWrapper = mount(<Table data={data} tableProperties={tableProperties} />)
+    expect(tableWrapper.containsMatchingElement(<div>John</div>)).toBeTruthy()
+  })
+
+  it('Pagination section is rendered correctly', () => {
+    const tableWrapper = mount(<Table data={data} tableProperties={tableProperties} />)
+    expect(tableWrapper.containsMatchingElement(<strong>1 of 1</strong>)).toBeTruthy()
+  })
+
+  it('Boolean column is rendered in green', () => {
+    const tableWrapper = mount(<Table data={data} tableProperties={tableProperties} />)
+    expect(
+      tableWrapper.containsMatchingElement(<div style={{ color: 'green' }}>true</div>),
+    ).toBeTruthy()
+  })
+
+  it('Pagination buttons are disabled due to results fitting in one page', () => {
+    const tableWrapper = mount(<Table data={data} tableProperties={tableProperties} />)
+    const paginationWrapper = tableWrapper.find('.pagination')
+    const disabledButton = paginationWrapper.find('button').first()
+    expect(disabledButton.props().disabled).toBe(true)
   })
 })
