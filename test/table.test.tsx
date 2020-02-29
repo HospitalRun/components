d28314b7806d22e2492ab176a0fd26f8e8f1e611
Stefano Casasola <stepanello@gmail.com>
2020-02-29 17:11:57 +0100

    feat: adds even more tests

--- test/table.test.tsx
+++ test/table.test.tsx
@@ -37,12 +37,25 @@ const tableProperties: any = {
       type: 'string',
       title: 'Nome',
       className: 'pl-3 fixoverflow',
+      headerClassName: 'text-info',
+      styles: [
+        {
+          style: {
+            fontWeight: '600',
+          },
+        },
+      ],
     },
     {
       accessor: 'admin',
       type: 'boolean',
       title: 'Admin',
       className: 'pl-3 fixoverflow',
+    },
+    {
+      accessor: 'date',
+      type: 'date',
+      title: 'Date',
     },
   ],
 }
@@ -63,6 +76,12 @@ describe('Table', () => {
     expect(tableWrapper.containsMatchingElement(<strong>1 of 1</strong>)).toBeTruthy()
   })
 
+  it('Column header classes are applied correctly', () => {
+    const tableWrapper = mount(<Table data={data} tableProperties={tableProperties} />)
+    const headerWrapper = tableWrapper.find('.text-info')
+    expect(headerWrapper).toBeTruthy()
+  })
+
   it('Boolean column is rendered in green', () => {
     const tableWrapper = mount(<Table data={data} tableProperties={tableProperties} />)
     expect(
@@ -75,5 +94,29 @@ describe('Table', () => {
     const paginationWrapper = tableWrapper.find('.pagination')
     const disabledButton = paginationWrapper.find('button').first()
     expect(disabledButton.props().disabled).toBe(true)
+  })
+
+  it('Column filter is rendered correctly', () => {
+    const tableWrapper = mount(<Table data={data} tableProperties={tableProperties} />)
+    const headerWrapper = tableWrapper.find('.text-info')
+    const inputWrapper = headerWrapper.find('input')
+    expect(inputWrapper).toBeTruthy()
+  })
+
+  it('Column styles are applied correctly', () => {
+    const tableWrapper = mount(<Table data={data} tableProperties={tableProperties} />)
+    const cellWrapper = tableWrapper.find('.fixoverflow')
+    const divWrapper = cellWrapper.find('div').first()
+    expect(divWrapper.props().style).toMatchObject({ fontWeight: '600' })
+  })
+
+  it('DatePicker filter is rendered correctly', () => {
+    const tableWrapper = mount(<Table data={data} tableProperties={tableProperties} />)
+    const datePickerWrapper = tableWrapper.find('.react-datepicker-wrapper')
+    const inputWrapper = datePickerWrapper.find('input')
+    expect(inputWrapper).toBeTruthy()
+    const prependWrapper = tableWrapper.find('.input-group-prepend')
+    const prependSpan = prependWrapper.find('span')
+    expect(prependSpan).toBeTruthy()
   })
 })
