0eb621ef150de2b6137f1dab1fbfe650c556ffba
Stefano Casasola <stepanello@gmail.com>
2020-03-01 16:19:04 +0100

    test(graph): adds some graph tests

--- test/bargraph.test.tsx
+++ test/bargraph.test.tsx
@@ -25,4 +25,75 @@ describe('BarGraph', () => {
 
     expect(wrapper.find(HTMLCanvasElement)).toHaveLength(1)
   })
+
+  it('BarGraph renders itself without crashing with time axis type', () => {
+    const wrapper = mount(
+      <BarGraph
+        title="Test Graph"
+        datasets={[
+          {
+            backgroundColor: 'blue',
+            label: 'Months',
+            data: [
+              { x: new Date(), y: 12 },
+              { x: new Date(), y: 11 },
+              { x: new Date(), y: 10 },
+            ],
+          },
+        ]}
+        xAxes={[{ label: 'Months', type: 'time' }]}
+        yAxes={[{ label: 'Numbers', type: 'linear' }]}
+      />,
+    )
+
+    expect(wrapper.find(HTMLCanvasElement)).toHaveLength(1)
+  })
+
+  it('BarGraph defaults axis type to category', () => {
+    const wrapper = mount(
+      <BarGraph
+        title="Test Graph"
+        datasets={[
+          {
+            backgroundColor: 'blue',
+            label: 'Months',
+            data: [
+              { x: 'January', y: 12 },
+              { x: 'February', y: 11 },
+              { x: 'March', y: 10 },
+            ],
+          },
+        ]}
+        xAxes={[{ label: 'Months' } as any]}
+        yAxes={[{ label: 'Numbers', type: 'linear' }]}
+      />,
+    )
+
+    expect(wrapper.find(HTMLCanvasElement)).toHaveLength(1)
+  })
+
+  it('BarGraph with fontSize and fontColor', () => {
+    const wrapper = mount(
+      <BarGraph
+        title="Test Graph"
+        titleFontSize={10}
+        titleFontColor="red"
+        datasets={[
+          {
+            backgroundColor: 'blue',
+            label: 'Months',
+            data: [
+              { x: 'January', y: 12 },
+              { x: 'February', y: 11 },
+              { x: 'March', y: 10 },
+            ],
+          },
+        ]}
+        xAxes={[{ label: 'Months' } as any]}
+        yAxes={[{ label: 'Numbers', type: 'linear' }]}
+      />,
+    )
+
+    expect(wrapper.find(HTMLCanvasElement)).toHaveLength(1)
+  })
 })
