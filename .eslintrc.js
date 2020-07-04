03d54ae64166045bc1237fa14d300ff3c5005f8f
Matteo Vivona <matteo.vivona@me.com>
2020-07-04 21:11:06 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -65,6 +65,10 @@ module.exports = {
     ],
     curly: ['error', 'all'],
     'react/require-default-props': ['warn'],
-    'react/default-props-match-prop-types': ['warn']
+    'react/default-props-match-prop-types': ['warn'],
+    "react/prop-types": [
+      "enabled",
+      { "ignore": "ignore", "customValidators": "customValidator" }
+    ]
   },
 }
