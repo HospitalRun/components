26d0fadd294810486f720c078e0f50fde7ee66ec
nischayv <nischay.v@gmail.com>
2020-07-15 20:07:09 -0700

    feat(callout): add a callout component
    feature #487

---
+++ src/components/Callout/Callout.tsx
@@ -0,0 +1,48 @@
+import classNames from 'classnames'
+import React, { CSSProperties } from 'react'
+
+import { ColorVariant } from '../../interfaces'
+import './callout.scss'
+
+interface Props {
+  /**
+   * Defines the title of the callout.
+   */
+  title?: string
+  /**
+   * Defines the color of the callout.
+   */
+  color?: ColorVariant
+  /**
+   * Defines the body of the callout.
+   */
+  children?: React.ReactElement
+  /**
+   * Styling through CSS classes
+   */
+  className?: string
+  /**
+   * Styilng through inline CSSProperties
+   */
+  style?: CSSProperties
+}
+
+export const Callout = ({
+  title,
+  color,
+  children,
+  className,
+  style,
+}: Props): React.ReactElement => {
+  const calloutClass = classNames(className, 'callout', `callout-${color}`)
+  return (
+    <div className={calloutClass} style={style}>
+      {title ? <h4>{title}</h4> : null}
+      {children}
+    </div>
+  )
+}
+
+Callout.defaultProps = {
+  color: 'primary',
+}
