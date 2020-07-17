e0410a8e5256a9d96914e8984367a07f271432ec
Nischay Venkatram <nischay.v@gmail.com>
2020-07-16 20:16:30 -0500

    feat(callout): add a callout component (#519)

---
+++ test/callout.test.tsx
@@ -0,0 +1,42 @@
+import { shallow } from 'enzyme'
+import * as React from 'react'
+
+import { Callout } from '../src'
+
+describe('Callout', () => {
+  it('Callout defaults color to primary', () => {
+    const calloutWrapper = shallow(<Callout title="This is a title" />)
+    expect(calloutWrapper.props().className).toMatch('callout-primary')
+  })
+
+  it('Callout can change colors', () => {
+    const calloutWrapper = shallow(<Callout color="secondary" />)
+    expect(calloutWrapper.props().className).toMatch('callout-secondary')
+  })
+
+  it('Callout can render title', () => {
+    const calloutWrapper = shallow(<Callout title="This is a title" />)
+    expect(calloutWrapper.find(HTMLHeadingElement)).toHaveLength(1)
+    expect(calloutWrapper.find(HTMLHeadingElement).text()).toEqual('This is a title')
+  })
+
+  it('Callout can render children elements', () => {
+    const calloutWrapper = shallow(
+      <Callout>
+        <p>Hello World</p>
+      </Callout>,
+    )
+    expect(calloutWrapper.find(HTMLParagraphElement)).toHaveLength(1)
+    expect(calloutWrapper.find(HTMLParagraphElement).text()).toEqual('Hello World')
+  })
+
+  it('Callout can use custom class', () => {
+    const calloutWrapper = shallow(<Callout className="customClass" />)
+    expect(calloutWrapper.props().className).toMatch('customClass')
+  })
+
+  it('Callout can use custom style', () => {
+    const calloutWrapper = shallow(<Callout style={{ background: 'red' }} />)
+    expect(calloutWrapper.props().style).toMatchObject({ background: 'red' })
+  })
+})
