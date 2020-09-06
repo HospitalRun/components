1bbf623597d74cfb80598d476be81b3418e47836
Ubuntu <ubuntu@ip-172-31-62-147.ec2.internal>
2020-09-06 16:13:03 +0000

    feat: 🎸 Adds addon-docs
    We update to Storybook 6. This inculdes addon-docs by default. This
    requires a full rewrite of .storybook files. No changes are made to
    stories or components (other than welcome.tsx).
    
    BREAKING CHANGE: 🧨 addon-info is replaced by addon-docs.
    
    ✅ Closes: 531

---
+++ .storybook/manager.js
@@ -0,0 +1,9 @@
+import { addons } from '@storybook/addons';
+import theme from './theme';
+
+addons.setConfig({
+  theme: theme,
+  showPanel: false,
+  isFullscreen: false,
+  isToolshown: true,
+});
